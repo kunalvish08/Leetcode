49f72398fa5efc7d16c78a6dd8b2275fcd71d01e
KUNAL VISHWAKARMA <kunalvishwakarma208@gmail.com>
2026-07-26 18:27:44 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,8 @@
- class Solution {
+class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0, mid = 0, high = nums.size() - 1;
+        int low = 0, mid = 0;
+        int high = nums.size() - 1;
 
         while (mid <= high) {
             if (nums[mid] == 0) {
